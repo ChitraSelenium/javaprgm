0dfca27b7ef45eac4b8463e30d0ad620a6b749ab
ChitraSelenium <jayachitdha23@gmail.com>
2021-04-28 09:53:21 +0530

    task completed

---
+++ src/test/java/org/com/FrameTask.java
@@ -0,0 +1,17 @@
+package org.com;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FrameTask {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\CHITRA\\eclipse\\ExcelTask\\Driver\\chromedriver.exe");
+	WebDriver driver= new  ChromeDriver();
+	driver.get("https://www.facebook.com/");
+	driver.findElement(By.id("email")).sendKeys("chitra");
+	driver.findElement(By.id("pass")).sendKeys("23533");
+	driver.findElement(By.name("login")).click();
+	
+}
+}
